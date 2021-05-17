1cc056e196a55808000a3bbb60c8d01a1bc27d16
root <y.s.lin@outlook.com>
2021-05-17 22:03:54 +0000

    add filtering in utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,20 @@ import matplotlib.pyplot as plt
 import os
 import re
 
+def get_config(path = './benchmark.conf'):
+    config = {}
+    with open(path) as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip()
+            if len(line) == 0 or line[0] == '#':
+                continue
+            print(line.strip().split('='))
+            k, v = line.strip().split('=')
+            config[k] = v
+    return config
+
+
 def get_latency_data(op):
     data = {}
     files = os.listdir('./')
@@ -17,11 +31,17 @@ def get_latency_data(op):
     return data
 
 
-def avg_latency_per_client(data):
+def avg_latency_per_client(data, filter=False):
     avg = {}
     for k, v in data.items():
         a = np.array(v)
         d = a[:, 1] - a[:, 0]
+        if filter:
+            mean = np.mean(d)
+            std = np.std(d)
+            mask = np.logical_and(d > mean-3*std, d < mean+3*std)
+            # print(mask)
+            d = d[mask]
         avg[k] = np.mean(d)
     return avg
 
@@ -47,13 +67,28 @@ def get_rate_data(op):
     return ts
 
 
-def avg_rate(data):
+def avg_rate(data, filter=-1):
+    # filter < 0, no filter at all
+    # ToDo, filter = 0, 3 sigma filtering
+    # filter > 0, throw away the first filter intervals for warm up
+
     last_timestamp = 0
     total_time = 0
     total_ops = 0
-    for c, r in data:
+    
+    _data = np.array(data)
+    if filter > 0:
+        last_timestamp = _data[filter-1, 0]
+        _data = _data[filter:, :]
+        
+    print(_data.shape, last_timestamp)
+    n, _ = _data.shape
+    for i in range(n):
+        c = _data[i][0] # current timestamp
+        r = _data[i][1] # rate
         interval = c - last_timestamp
         total_time += interval
         total_ops += interval * r
         last_timestamp = c
+        print(total_time, total_ops)
     return total_ops / total_time
